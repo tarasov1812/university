b36436bf18c2b0568bf589012a0c7ba8e95f6ef0
Alexandr Tarasov <75462526+tarasov1812@users.noreply.github.com>
2024-06-13 18:30:08 +0200

    create empty spring boot thymeleaf project

---
+++ src/main/java/es/university/ugr/UgrApplication.java
@@ -0,0 +1,13 @@
+package es.university.ugr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UgrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UgrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/university/ugr/UgrApplicationTests.java
@@ -0,0 +1,13 @@
+package es.university.ugr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UgrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
