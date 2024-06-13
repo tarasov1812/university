1a9b6378cbc269259b10a97d5914884a7e872ac0
Alexandr Tarasov <75462526+tarasov1812@users.noreply.github.com>
2024-06-13 19:08:59 +0200

    Hello world controller

---
+++ src/main/java/es/university/ugr/controller/HelloWorldController.java
@@ -0,0 +1,14 @@
+package es.university.ugr.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloWorldController {
+    @GetMapping("/hello-world")
+    public String helloWorld(Model model){
+        model.addAttribute("message", "Hello world");
+        return "hello-world";
+    }
+}
